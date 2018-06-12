5289c63302af4067d5e50a61f3efc502fb7f8683
atsushi10mino <msis.cu10@gmail.com>
2018-06-12 14:06:10 +0900

    rvizはセンサデータが返ってこない場合,番号をskipするっぽい

--- scan_intensity/src/scan_intensity_sub.cpp
+++ scan_intensity/src/scan_intensity_sub.cpp
@@ -3,12 +3,17 @@
 
 void intensityCallback(const sensor_msgs::LaserScan &msg)
 {
+	int count = 0;
 	for(int i =0 ; i < 720 ;  i++)
 	{		
-		if(msg.intensities[i]>10000)
-			ROS_INFO("number:%d,intensities: %f",i,msg.intensities[i]);
+		/*if(msg.intensities[i]>10000)*/
+			ROS_INFO("number:%d,intensities:%f",i,msg.intensities[i]);
+			ROS_INFO("number:%d,ranges:%f",i,msg.ranges[i]);
+			if(msg.intensities[i] == 0 && msg.ranges[i] <= 0.011)
+				count++;
+			
 	}
-	ROS_INFO("hogehoge");
+	ROS_INFO("hogehoge,%d",count);
 }
 
 int main(int argc, char **argv)
