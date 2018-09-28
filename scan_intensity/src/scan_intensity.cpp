7808852f06e8aa48c92c409e212374976efb1086
atsushi10mino <msis.cu10@gmail.com>
2018-09-28 13:19:25 +0900

    条件の緩和

--- scan_intensity/src/scan_intensity.cpp
+++ scan_intensity/src/scan_intensity.cpp
@@ -3,8 +3,12 @@
 
 
 ros::Publisher intensity_point_pub;
-sensor_msgs::LaserScan strong;
-
+sensor_msgs::LaserScan strong;//,adjust;
+/*
+void intensity_adjustment(float range)
+{
+	float adjust = range;//hakattekimero
+}*/
 void intensityCallback(const sensor_msgs::LaserScan &msg)
 {
 	
@@ -21,12 +25,19 @@ void intensityCallback(const sensor_msgs::LaserScan &msg)
 	//strong.ranges.clear();
 	//strong.intensities.clear();
 	
+/*	adjust = msg;
+	for(int gensui = 0 ; gensui < adjust.ranges.size() ; gensui++ )
+	{
+		if(adjust.intensities[gensui] > 6000 && adjust.ranges[gensui] > 1)
+			adjust.intensities[gensui] *= 1.5;  
+	}*/
+	
 	float max_intensity = *std::max_element(msg.intensities.begin(),msg.intensities.end());
 	float min_intensity = *std::min_element(msg.intensities.begin(),msg.intensities.end());
 
 	for(int i = 0 ; i < strong.ranges.size() ;  i++)
 	{		
-		if(max_intensity > 8000 && (msg.intensities[i] - min_intensity) / (max_intensity - min_intensity) > 0.9)
+		if(max_intensity > 10000 && (msg.intensities[i] - min_intensity) / (max_intensity - min_intensity) > 0.65)
 		{	
 			strong.ranges.insert(strong.ranges.begin() + i, msg.ranges[i]);
 			strong.intensities.insert(strong.intensities.begin() + i, msg.intensities[i]);
@@ -50,7 +61,7 @@ int main(int argc, char **argv)
 	
 	ros::Subscriber intensity_sub = nh.subscribe("scan",100,intensityCallback);
 	
-	intensity_point_pub = nh.advertise<sensor_msgs::LaserScan>("strong_reflction_point",100);
+	intensity_point_pub = nh.advertise<sensor_msgs::LaserScan>("strong_reflection_point",100);
 	
 	ros::Rate loop_rate(100);
 	
